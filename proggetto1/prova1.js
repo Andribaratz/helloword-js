456e7a2cccbfa26cf55b534d678fd02671cc1df4
Andribaratz <andrea.barattini@donorionefano.edu.it>
2025-05-19 16:59:46 +0100

    rinminata cartella

--- proggetto1/prova1.js
+++
@@ -1,6 +0,0 @@
-function calcolasomma(){
-let numero1=parseFloat(document.getElementById("numero1").value);
-let numero2=parseFloat(document.getElementById("numero2").value);
-let somma = numero1 + numero2;
-document.getElementById("risultato").innerText = "la somma e: " + somma;
-}
