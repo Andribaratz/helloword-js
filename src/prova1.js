46b831530e965af4661630c9cb7f0305bc870b0d
sini022 <fastasticcici@gmail.com>
2025-05-20 12:03:04 +0200

    aggiunto scrittura

--- src/prova1.js
+++ src/prova1.js
@@ -6,3 +6,15 @@ document.getElementById("risultato").innerText = "la somma e: " + somma;
 }
 console.log("ciaoo mondo");
 console.log("come stai?");
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Scrivi qualcosa: ", function (risposta) {
+    console.log("Hai scritto:", risposta);
+    rl.close();
+});
